ed59c71dc39072284b47e71b89bc1dd9507ec1f9
Vensan Cabardo <42554910+vcabardo@users.noreply.github.com>
2022-03-14 12:25:32 -0600

    Adding todos for the project

--- classification.py
+++ classification.py
@@ -1,3 +1,25 @@
+# TODO: import libraries
 import pandas as pd
 
-data = pd.read_csv("Dry_Bean_Dataset.csv")
+df = pd.read_csv("Dry_Bean_Dataset.csv")
+
+#TODO: check for null values in dataset. if that is the case, average values in
+#row and use result to fill null cells
+# print(df.isnull().sum()) #No need to work on null vals
+
+#TODO: perform feature scaling on the data (note: not necessary if we do
+#decision trees unless we're doing dimensionality reduction)
+
+#TODO: split data into training and testing (use cross validation?)
+
+#TODO: determine if we need to use all of the features, or if omitting any
+#leads to higher accuracy
+
+#TODO: train the model (SVM? DT? MLP? Multiple models?)
+
+#TODO: Hyperparameter tuning with validation/learning curves and/or gridsearch?
+
+#TODO: use the model to make predictions
+
+#TODO: Obtain performance metrics (runtime, accuracy, precision, recall, f1, etc.)
+#for each model, make comparisons
